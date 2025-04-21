5f58758cab202957d9776307116c00b6d1907edc
xalabama <20598360+xalabama@users.noreply.github.com>
2025-04-21 21:54:26 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,9 @@
 
 namespace AmaizingCompany\CannaleoClient\Tests;
 
+use AmaizingCompany\CannaleoClient\CannaleoClientServiceProvider;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
-use AmaizingCompany\CannaleoClient\CannaleoClientServiceProvider;
 
 class TestCase extends Orchestra
 {
