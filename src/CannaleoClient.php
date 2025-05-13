5ba2093fe2bab0c3959bb093e56c80866bb9a9b1
Sandro Rümmler <kontakt@sandro-ruemmler.de>
2025-05-13 16:01:54 +0200

    Add service methods to package manager

--- src/CannaleoClient.php
+++ src/CannaleoClient.php
@@ -2,6 +2,19 @@
 
 namespace AmaizingCompany\CannaleoClient;
 
+use AmaizingCompany\CannaleoClient\Api\Requests\CatalogRequest;
+use AmaizingCompany\CannaleoClient\Api\Requests\PharmaciesRequest;
+use AmaizingCompany\CannaleoClient\Contracts\Models\CannaleoCustomer;
+use AmaizingCompany\CannaleoClient\Contracts\Models\CannaleoDoctor;
+use AmaizingCompany\CannaleoClient\Contracts\Models\CannaleoOrder;
+use AmaizingCompany\CannaleoClient\Contracts\Models\CannaleoPrescription;
+use AmaizingCompany\CannaleoClient\Contracts\Models\Pharmacy;
+use AmaizingCompany\CannaleoClient\Services\PrescriptionTransactionService;
+use AmaizingCompany\CannaleoClient\Services\SyncServices\CatalogSyncService;
+use AmaizingCompany\CannaleoClient\Services\SyncServices\PharmaciesSyncService;
+use Illuminate\Support\Collection;
+use Throwable;
+
 class CannaleoClient
 {
     public function getConfig(?string $key = null): mixed
@@ -11,5 +24,51 @@ class CannaleoClient
         }
 
         return config('cannaleo-client'.$key);
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function syncCatalog(): void
+    {
+        $request = new CatalogRequest();
+        $service = new CatalogSyncService($request);
+
+        $service->sync();
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function syncPharmacies(): void
+    {
+        $request = new PharmaciesRequest();
+        $service = new PharmaciesSyncService($request);
+
+        $service->sync();
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function sendPrescription(
+        Pharmacy $pharmacy,
+        CannaleoPrescription $prescription,
+        CannaleoCustomer $customer,
+        CannaleoDoctor $doctor,
+        CannaleoOrder $order,
+        Collection $products
+    ): void
+    {
+        $service = new PrescriptionTransactionService(
+            $pharmacy,
+            $prescription,
+            $customer,
+            $doctor,
+            $order,
+            $products
+        );
+
+        $service->handle();
     }
 }
--- src/Facades/CannaleoClient.php
+++ src/Facades/CannaleoClient.php
@@ -2,12 +2,21 @@
 
 namespace AmaizingCompany\CannaleoClient\Facades;
 
+use AmaizingCompany\CannaleoClient\Contracts\Models\CannaleoCustomer;
+use AmaizingCompany\CannaleoClient\Contracts\Models\CannaleoDoctor;
+use AmaizingCompany\CannaleoClient\Contracts\Models\CannaleoOrder;
+use AmaizingCompany\CannaleoClient\Contracts\Models\CannaleoPrescription;
+use AmaizingCompany\CannaleoClient\Contracts\Models\Pharmacy;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Facade;
 
 /**
  * @see \AmaizingCompany\CannaleoClient\CannaleoClient
  *
  * @method static mixed getConfig(?string $key = null)
+ * @method static void syncCatalog()
+ * @method static void syncPharmacies()
+ * @method static void sendPrescription(Pharmacy $pharmacy, CannaleoPrescription $prescription, CannaleoCustomer $customer, CannaleoDoctor $doctor, CannaleoOrder $order, Collection $products)
  */
 class CannaleoClient extends Facade
 {
