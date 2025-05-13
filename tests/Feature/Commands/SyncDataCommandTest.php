fff1342e1cde8aace83d6cc32ab990a00ea4720c
Sandro Rümmler <kontakt@sandro-ruemmler.de>
2025-05-13 16:10:51 +0200

    Remove obsolete tests

--- tests/Feature/Commands/SyncDataCommandTest.php
+++ tests/Feature/Commands/SyncDataCommandTest.php
@@ -1,26 +1,10 @@
 <?php
 
 use AmaizingCompany\CannaleoClient\Api\Enums\Endpoint;
-use AmaizingCompany\CannaleoClient\Services\SyncServices\CatalogSyncService;
-use AmaizingCompany\CannaleoClient\Services\SyncServices\PharmaciesSyncService;
 use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Log;
 
 test('command can be executed', function () {
     $this->fakeHttpResponses();
-
-    $this->artisan('cannaleo:sync')
-        ->assertSuccessful();
-});
-
-test('command executes synchronization', function () {
-    $this->fakeHttpResponses();
-
-    Log::shouldReceive('info')
-        ->with(
-            PharmaciesSyncService::getSuccessMessage(),
-            CatalogSyncService::getSuccessMessage()
-        );
 
     $this->artisan('cannaleo:sync')
         ->assertSuccessful();
